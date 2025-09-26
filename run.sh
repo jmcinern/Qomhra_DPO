befc88cb41a8155d8f1ec88e23f6e0c48cf43fc9
joseph <jmcinern@tcd.ie>
2025-09-26 18:00:17 +0100

    full train

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=quant_dpo
 #SBATCH --output=./out/quant_dpo_%j.out
 #SBATCH --error=./err/quant_dpo_%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=02:30:00
 #SBATCH --partition=k2-gpu-v100
 #SBATCH --gres=gpu:v100:1
 #SBATCH --ntasks=1
@@ -13,5 +13,7 @@
 
 pip install -r requirements.txt
 # run it
-accelerate launch dpo_train.py
+accelerate launch dpo_train.py --num_processes=1 --num_machines=1 --mixed_precision='no' --dynamo_backend='no'
+
+
 
